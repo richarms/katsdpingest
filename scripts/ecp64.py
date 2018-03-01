2c206ee639340582d36556d841d2e84fabaa27f4
Ludwig Schwardt <ludwig@ska.ac.za>
2018-03-01 15:41:22 +0200

    Newest version of demo ECP-64 receiver
    This was obtained from Vereese. It removes the explicit descriptors
    (now produced by DMC) and plotting framework, connects to an IP
    specified on the command line and splits the power data into 'on'
    and 'off' bits.

--- scripts/ecp64.py
+++ scripts/ecp64.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import logging
 from collections import OrderedDict
+import sys
 
 import numpy as np
 import spead2
@@ -53,83 +54,23 @@ def unpack_bits(x, partition):
     return out[::-1]    # Put back into MSB-to-LSB order
 
 
-def signal_display(freqs, test_tone):
-    """Create the shell of a live signal display of the ECP-64 data."""
-    plt.close('all')
-    fig, axgrid = plt.subplots(2, 2, num=1)
-    axgrid[0][0].set_title('HH')
-    axgrid[0][0].set_xticklabels([])
-    axgrid[0][0].plot(freqs / 1e6, 30 * np.ones_like(freqs))
-    axgrid[0][0].axvline(test_tone / 1e6, color='k', linestyle='--')
-    axgrid[0][0].set_xlim(freqs[0] / 1e6, freqs[-1] / 1e6)
-    axgrid[0][0].set_ylim(45, 65)
-    axgrid[0][0].set_ylabel('Power (dB)')
-    axgrid[0][1].set_title('VV')
-    axgrid[0][1].set_xticklabels([])
-    axgrid[0][1].yaxis.tick_right()
-    axgrid[0][1].yaxis.set_label_position('right')
-    axgrid[0][1].plot(freqs / 1e6, 30 * np.ones_like(freqs))
-    axgrid[0][1].axvline(test_tone / 1e6, color='k', linestyle='--')
-    axgrid[0][1].set_xlim(freqs[0] / 1e6, freqs[-1] / 1e6)
-    axgrid[0][1].set_ylim(45, 65)
-    axgrid[0][1].set_ylabel('Power (dB)')
-    axgrid[1][0].set_title('ReVH')
-    axgrid[1][0].set_xlabel('Frequency (MHz)')
-    axgrid[1][0].plot(freqs / 1e6, np.zeros_like(freqs))
-    axgrid[1][0].axvline(test_tone / 1e6, color='k', linestyle='--')
-    axgrid[1][0].set_xlim(freqs[0] / 1e6, freqs[-1] / 1e6)
-    axgrid[1][0].set_ylim(-500000, 500000)
-    axgrid[1][0].set_ylabel('Amplitude (linear)')
-    axgrid[1][1].set_title('ImVH')
-    axgrid[1][1].yaxis.tick_right()
-    axgrid[1][1].yaxis.set_label_position('right')
-    axgrid[1][1].plot(freqs / 1e6, np.zeros_like(freqs))
-    axgrid[1][1].axvline(test_tone / 1e6, color='k', linestyle='--')
-    axgrid[1][1].set_xlim(freqs[0] / 1e6, freqs[-1] / 1e6)
-    axgrid[1][1].set_ylim(-500000, 500000)
-    axgrid[1][1].set_xlabel('Frequency (MHz)')
-    axgrid[1][1].set_ylabel('Amplitude (linear)')
-    return fig, axgrid
-
-
 if __name__ == '__main__':
 
-    ip = '239.10.3.10'
+    ip = sys.argv[1]
     ports = (7150, 7151, 7152, 7153)
     num_chans = 128
     sampling_rate = 1712e6
     bandwidth = 0.5 * sampling_rate
     centre_freq = 0.75 * sampling_rate
-    test_tone = 912e6
 
     chans = channel_ordering(num_chans)
-    freqs = centre_freq + bandwidth / num_chans * (np.arange(num_chans) - num_chans / 2)
+    freqs = centre_freq + (bandwidth / num_chans) * (np.arange(num_chans) - num_chans / 2)
 
     logging.basicConfig(level='DEBUG')
     rx = recv.Stream(spead2.ThreadPool())
     for port in ports:
         rx.add_udp_reader(port, bind_hostname=ip)
     ig = spead2.ItemGroup()
-
-    # Hack to create descriptors locally (Marc's DMC will provide them eventually)
-    ig.add_item(id=0x1600, name='timestamp',
-                description='Local digitiser timestamp at start of accumulation',
-                shape=(), format=[('u', 48)])
-    ig.add_item(id=0x3101, name='digitiser_id',
-                description='Digitiser serial number, type, receptor ID, pol ID',
-                shape=(), format=[('u', 48)])
-    ig.add_item(id=0x3102, name='digitiser_status',
-                description='Accumulator / FFT / ADC saturation, noise diode status',
-                shape=(), format=[('u', 48)])
-    ig.add_item(id=0x3301, name='data_vv',
-                description='Autocorrelation VV*',
-                shape=(num_chans,), dtype='>u4')
-    ig.add_item(id=0x3302, name='data_hh',
-                description='Autocorrelation HH*',
-                shape=(num_chans,), dtype='>u4')
-    ig.add_item(id=0x3303, name='data_vh',
-                description='Crosscorrelation VH* (real followed by imag)',
-                shape=(2 * num_chans,), dtype='>i4')
 
     raw_data = OrderedDict()
     for heap in rx:
@@ -143,7 +84,7 @@ if __name__ == '__main__':
         saturation, nd_on = unpack_bits(dig_status, (8, 1))
         stream = [s[5:] for s in new_items if s.startswith('data_')][0]
         print(receptor, timestamp, nd_on, stream)
-        key = (receptor, timestamp)
+        key = (receptor[0], timestamp[0])
         fields = raw_data.get(key, {})
         fields['saturation'] = saturation
         fields['nd_on'] = nd_on
@@ -157,9 +98,13 @@ if __name__ == '__main__':
     timestamps = []
     saturation = []
     nd_on = []
-    data_hh = []
-    data_vv = []
-    data_vh = []
+    data_hh_0 = []
+    data_vv_0 = []
+    data_vh_0 = []
+    data_hh_1 = []
+    data_vv_1 = []
+    data_vh_1 = []
+
     for key, value in raw_data.items():
         receptor = 'm%03d' % (key[0],)
         if len(value) < 6:
@@ -167,10 +112,15 @@ if __name__ == '__main__':
         timestamps.append(key[1])
         nd_on.append(value['nd_on'])
         saturation.append(value['saturation'])
-        data_hh.append(value['hh'])
-        data_vv.append(value['vv'])
-        data_vh.append(value['revh'] + 1.0j * value['imvh'])
+        if value['nd_on'] == 0 :
+            data_hh_0.append(value['hh'])
+            data_vv_0.append(value['vv'])
+            data_vh_0.append(value['revh'] + 1.0j * value['imvh'])
+        else:
+            data_hh_1.append(value['hh'])
+            data_vv_1.append(value['vv'])
+            data_vh_1.append(value['revh'] + 1.0j * value['imvh'])
 
-    np.savez('ecp64.npz', ts=timestamps, sat=saturation, nd=nd_on,
-             hh=data_hh, vv=data_vv, vh=data_vh, freqs=freqs,
+    np.savez('spectrometer_data.npz', ts=timestamps, sat=saturation, nd=nd_on,
+             hh_0=data_hh_0, vv_0=data_vv_0, vh_0=data_vh_0, hh_1=data_hh_1, vv_1=data_vv_1, vh_1=data_vh_1, freqs=freqs,
              ds=dig_serial, dt=dig_type, receptor=receptor)
